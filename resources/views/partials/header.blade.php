3e9eeaf5ff5ee80465b87037584ae692ee3aedfe
byron Fichardt <byron@Byron-ltp.TREELINE.CO.ZA>
2021-07-04 13:37:07 +0200

    add analytics

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -22,4 +22,13 @@
             }
         }
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CQZTVKW8B7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-CQZTVKW8B7');
+    </script>
 </head>
